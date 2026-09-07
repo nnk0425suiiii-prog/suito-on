c5a19592083757e5420dc1717a2451633cdbc8b5
nnk0425suiiii-prog <nnk0425suiiii@gmail.com>
2026-09-07 14:27:53 +0900

    Update home-guide.js

--- home-guide.js
+++ home-guide.js
@@ -63,19 +63,44 @@ if (
   /* ==================================================
      最新1件
   ================================================== */
+const latestGuidePost =
+  [...guidePosts]
 
-  const latestGuidePost =
-    [...guidePosts]
+    /* 未公開の記事はHOMEに出さない */
+    .filter(post => {
 
-      .sort(
-        (a, b) =>
-          new Date(b.datetime)
-          -
-          new Date(a.datetime)
-      )
+      if (!post.datetime) {
+        return false;
+      }
 
-      [0];
 
+      const publishDate =
+        post.queue === true
+
+          ? new Date(
+              `${post.datetime}T10:00:00+09:00`
+            )
+
+          : new Date(
+              `${post.datetime}T00:00:00+09:00`
+            );
+
+
+      return (
+        publishDate <= new Date()
+      );
+
+    })
+
+    /* 公開済みの中から最新 */
+    .sort(
+      (a, b) =>
+        new Date(b.datetime)
+        -
+        new Date(a.datetime)
+    )
+
+    [0];
 
 
   /* ==================================================
